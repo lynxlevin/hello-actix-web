5eb306902d7af78bafa6c4bb8f45a36bd8de7096
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-06-23 11:13:21 +0900

    State

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,24 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+struct AppState {
+    app_name: String,
+}
+
+#[get("/index")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
             .service(hello)
             .service(web::scope("/app").service(echo))
             .route("/hey", web::get().to(manual_hello))

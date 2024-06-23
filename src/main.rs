99b9e42d99bd63983d51bf602b79e26f18c86211
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-06-23 11:19:43 +0900

    Shared mutable state

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -24,17 +25,35 @@ async fn index(data: web::Data<AppState>) -> String {
     format!("Hello {app_name}!")
 }
 
+struct MutableState {
+    counter: Mutex<i32>,
+}
+
+#[get("/counter")]
+async fn mutable_counter(data: web::Data<MutableState>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Request number: {counter}")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(MutableState {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(AppState {
                 app_name: String::from("Actix Web"),
             }))
+            .app_data(counter.clone())
             .service(index)
             .service(hello)
             .service(web::scope("/app").service(echo))
             .route("/hey", web::get().to(manual_hello))
+            .service(web::scope("/shared-mutable-state").service(mutable_counter))
     })
     .bind(("127.0.0.1", 8080))?
     .run()

c7df07035b3a3583cc31f8d969e866ef2992c25a
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-07-13 21:20:34 +0900

    get_all API

--- src/todo_app/mod.rs
+++ src/todo_app/mod.rs
@@ -1,10 +1,24 @@
-use actix_web::{post, web, Responder, Result};
+use actix_web::{get, post, web, Responder, Result};
 use serde::Deserialize;
 
-use crate::diesel_code::{create_todo, establish_connection};
+use crate::diesel_code::{create_todo, establish_connection, models::Todo};
+use diesel::prelude::*;
 
 pub fn todo_app_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/todo").service(create));
+    cfg.service(web::scope("/todo").service(get_all).service(create));
+}
+
+#[get("")]
+async fn get_all() -> Result<impl Responder> {
+    use crate::diesel_code::schema::todos::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = todos
+        .select(Todo::as_select())
+        .load(connection)
+        .expect("Error loading todos");
+
+    Ok(web::Json(results))
 }
 
 #[derive(Deserialize)]
